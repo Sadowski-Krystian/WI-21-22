1ae704a9a85aec546e2cc38a30e71f2bc301bfe6
Sadowski <krystek23s@gmail.com>
2022-05-24 13:22:55 +0200

    [add]

--- js-powturzenie/main.js
+++ js-powturzenie/main.js
@@ -1,7 +1,27 @@
 function hello(params) {
     console.log(params);
+    let n = document.getElementsByTagName('nav')[0]
+    let a = n.querySelectorAll('[data-target]')
+    console.log(a);
+    for (let i = 0; i < a.length; i++) {
+        a[i].addEventListener('click', toggleSect, false)
+        
+    }
 }
-
+let toggleSect = (e)=>{
+    let btn = e.target.closest('a')
+    let trgt = btn.dataset.target
+    console.log(btn);
+    let box = document.getElementById('view')
+    let sect = box.querySelectorAll('[data-sect]')
+    for (let i = 0; i < sect.length; i++) {
+        if(sect[i].dataset.sect == trgt){
+            sect[i].classList.add('doDsp')
+        }else{
+            sect[i].classList.remove('doDsp')
+        }
+    }
+}
 let hello2 = function hello2(params) {
     console.log(params);
 }
